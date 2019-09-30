03b26e18732e411df6d9eabedfbe2c8a25fbac20
nikita202 <nikita@ostaplyuk.com>
2019-09-30 21:49:14 +0300

    Initial

---
+++ src/Problem_1/MapWindowController.java
@@ -0,0 +1,19 @@
+package Problem_1;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MapWindowController implements Initializable {
+    @FXML MapView mapView;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public MapView getMapView() {
+        return mapView;
+    }
+}
