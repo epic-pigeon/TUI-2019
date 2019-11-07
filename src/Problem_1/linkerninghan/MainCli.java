6bbd3e97a27d770ef66664b2488524d2173cc666
triangle-square <triangle-square>
2019-11-07 19:03:32 +0200

    [T1] Add Lin-Kernighan TSP problem solution

---
+++ src/Problem_1/linkerninghan/MainCli.java
@@ -0,0 +1,51 @@
+package Problem_1.linkerninghan;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MainCli {
+
+    private static final int ITERATIONS_NUM = 100;
+
+    public static void main(String[] args) {
+        // Generate points that are located on each node of a grid of given width and height.
+        // If either width or height is even, min distance to visit all points is [width * height].
+        // If both width and height are odd, min distance is [width * height - 1 + sqrt(2)].
+        ArrayList<Point> coordinates = generateGridPoints(5, 5);
+        ArrayList<Integer> ids = new ArrayList<>(Stream.iterate(1, i -> i + 1).limit(coordinates.size())
+                .collect(Collectors.toList()));
+
+        // Time tracking.
+        long startTime = System.currentTimeMillis();
+
+        LinKernighan result = runLinKernighan(coordinates, ids);
+
+        System.out.printf("The solution took: %dms\n", System.currentTimeMillis() - startTime);
+        System.out.println("The solution is: ");
+        System.out.println(result);
+    }
+
+    public static LinKernighan runLinKernighan(ArrayList<Point> coordinates, ArrayList<Integer> ids) {
+        LinKernighan bestResult = new LinKernighan(coordinates, ids);
+        for (int i = 0; i < ITERATIONS_NUM; i++) {
+            LinKernighan lk = new LinKernighan(coordinates, ids);
+            lk.runAlgorithm();
+            if (lk.getDistance() < bestResult.getDistance()) {
+                bestResult.tour = lk.tour.clone();
+            }
+        }
+        return bestResult;
+    }
+
+    public static ArrayList<Point> generateGridPoints(int height, int width) {
+        ArrayList<Point> coords = new ArrayList<>();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                coords.add(new Point(i, j));
+            }
+        }
+        return coords;
+    }
+}
+
---
+++ src/Problem_1/linkerninghan/Point.java
@@ -0,0 +1,47 @@
+package Problem_1.linkerninghan;
+
+/**
+ * This class represents a cartesian 2D point using its x and y coordinates
+ */
+public class Point {
+    /*
+     * Instance variables
+     */
+
+    // The x coordinate
+    private double x;
+
+    // The y coordinate
+    private double y;
+
+    /**
+     * This is the constructor that takes both coordinates and creates a point
+     *
+     * @param x the x coordinates
+     * @param y the y coordinates
+     */
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * A getter that returns the x coordinates
+     *
+     * @return double the x coordinates
+     */
+    public double getX() {
+        return this.x;
+    }
+
+    /**
+     * A getter that returns the y coordinates
+     *
+     * @return double the y coordinates
+     */
+    public double getY() {
+        return this.y;
+    }
+
+
+}
